27ee5410d7f912b3d765f22613dae186ee7f0350
Bobby Schade <bschade.18@gmail.com>
2019-12-04 07:26:38 -0600

    Updates

---
+++ precache-manifest.a8ec48169f60feec29e9afe159561eea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726c9f54918d277c477f2e612aa3bcea",
+    "url": "/mern-exercise-tracker/index.html"
+  },
+  {
+    "revision": "14eda605b822205bdb0d",
+    "url": "/mern-exercise-tracker/static/css/2.821458bd.chunk.css"
+  },
+  {
+    "revision": "14eda605b822205bdb0d",
+    "url": "/mern-exercise-tracker/static/js/2.9b959c89.chunk.js"
+  },
+  {
+    "revision": "f79caaffba4f890c8dec",
+    "url": "/mern-exercise-tracker/static/js/main.affdb4fe.chunk.js"
+  },
+  {
+    "revision": "3dfcb643c0a6a736ebfb",
+    "url": "/mern-exercise-tracker/static/js/runtime-main.add8b367.js"
+  }
+]);
